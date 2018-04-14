3fb39648d45cbcedca1e9225b11116a0b1fdceca
Joe Booth <joseph.booth3@gmail.com>
2018-04-14 08:42:02 -0500

    display end of game messages

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,108 +1,115 @@
 //define variables
 
-var gameOn = false;
-var correctAnswer = false;
 var timeUp = false;
 var gameOver = false;
-var myQuestions = [];
 var rightAnswer = 0;
-var wrongAnswer = 0;
-
-
-
-$(document).ready(function() {
-    //start button is clicked
-    $("#start").click(function() {
-        questionList.gameOn();
-    })
-
-    getQuestions();
-
-    $(".buttons").click(function() {
-        questionList.gradeAnswer($(this).data("answer"));
-        console.log("graded it!");
-    });
-
-    //resets all variables
-    function reset() {
-        gameActive = false,
-            correctAnswer = false,
-            gameOver = false,
-            questionList.num = 8,
-            rightAnswer = 0,
-            wrongAnswer = 0,
-            gameQuestion = [];
-        timer.reset();
-        getQuestions();
-        questionList.gameOn();
-    }
-    //array of all the questions and answers
-    var myQuestions = [
+var currentQuestion = 0;
+var myQuestions = [
     {
-        quest: "Lorem ipsum?",
-        answ: ["a", "b", "c", "d"],
+        quest: "Where is The Wire set?",
+        answ: ["Baltimore,MD", "Philadelphia, PA", "Boston, MA", "New York, NY"],
         answIndex: 0
     },
 
     {
-        quest: "Lorem ipsum?",
-        answ: ["a", "b", "c", "d"],
-        answIndex: 0
+        quest: "What town is Stranger Things set in?",
+        answ: ["Elgin, IL", "Farmington, MO", "Lincoln, NE", "Hawkins, IN"],
+        answIndex: 3
     },
 
     {
-        quest: "Lorem ipsum?",
-        answ: ["a", "b", "c", "d"],
-        answIndex: 0
+        quest: "Where is the American version of The Office set?",
+        answ: ["Dayton, OH", "Edina, MN", "West Lafayette, IN", "Scranton, PA"],
+        answIndex: 3
     },
 
     {
-        quest: "Lorem ipsum?",
-        answ: ["a", "b", "c", "d"],
-        answIndex: 0
+        quest: "Where is Breaking Bad set?",
+        answ: ["Moab, UT", "Albuquerque, NM", "Phoenix, AZ", "Fresno, CA"],
+        answIndex: 1
     },
 
     {
-        quest: "Lorem ipsum?",
-        answ: ["a", "b", "c", "d"],
-        answIndex: 0
+        quest: "What city is Cheers set in?",
+        answ: ["Philadelphia, PA", "Chicago, IL", "Providence, RI", "Boston, MA"],
+        answIndex: 3
     },
 
     {
-        quest: "Lorem ipsum?",
-        answ: ["a", "b", "c", "d"],
-        answIndex: 0
+        quest: "What town is Parks and Recreation set in?",
+        answ: ["Springfield, IL", "Utica, NY", "Pawnee, IN", "Erie, PA"],
+        answIndex: 2
     },
 
     {
-        quest: "Lorem ipsum?",
-        answ: ["a", "b", "c", "d"],
-        answIndex: 0
+        quest: "What city is Boardwalk Empire set in?",
+        answ: ["Santa Monica, CA", "Chicago, IL", "Atlantic City, NJ", "Ocean City, MD"],
+        answIndex: 2
     },
 
     {
-        quest: "Lorem ipsum?",
-        answ: ["a", "b", "c", "d"],
-        answIndex: 0
+        quest: "What city is Happy Days set in?",
+        answ: ["Minneapolis, MN", "Milwaukee, WI", "St Louis, MO", "Detroit, MI"],
+        answIndex: 1
     }
 
     ];
-    })
+
+
+$(document).ready(function() {
+    //questions and answers start off hidden
+    
+    //start button is clicked.  Trying to reset display property so questions and answers are shown
+    $("#start").click(function() {
+        displayCurrentQuestion();
+        timer.run();
+        $(".quizMessage").hide();
+        $("next").on("click", function() {
+            if(!gameOver) {
+    //checking user input
+                value = $("input[type='radio']:checked").val();
+    //checking user input against correct answer
+                if (value == myQuestions[currentQuestion].answIndex) {
+                    correctAnswers++;
+                }
+    //move to next question
+                currentQuestion++;
+    //checking game progress
+                if (currentQuestion < questions.length) {
+                    displayCurrentQuestion();
+                } else {
+
+                    
+    //showing score and resetting the game
+                    displayScore();
+                    gameOver = true;
+                    timer.reset();
+                    reset();
+                
+                }
+            }        
+        })
+        
+    
 
    
 
-function getQuestions() {
-    //make questions into new array
-    gameQuestion = myQuestions.slice(0);
-    for (var i = myQuestions.length - 1; i > 0; i--) {
-        
-        var getIndex = Math.floor(Math.random() * (i + 1));
+    //resets all variables
+    function reset() {
+        var timeUp = false;
+        var gameOver = false;
+        var rightAnswer = 0;
+        var currentQuestion = 0;
+        timer.reset();
+        getQuestions();
 
-        gameQuestion[getIndex] = gameQuestion[i];
-
-        gameQuestion[i] = displayQuestion;
+    //show current question
+    function displayCurrentQuestion() {
+        var question = questions[currentQuestion].question;
+        $("#question").text(myQuestions.question.quest);
+        $(".options").text(myQuestions.question.answ);
     }
-}
+       
 
 //set timer
 var timer = {
@@ -113,15 +120,15 @@ var timer = {
 
         $("#timer").text("120");
     },
-//stop timer
+//begin timer
     begin: function() {
         cycle = setInterval(timer.run, 1000);
     },
-
+//stop timer
     stop: function() {
         clearInterval(cycle);
     },
-
+//funtion to run timer until time hits zero
     run: function() {
         if (timer.clock === 0) {
             timeUp = true;
@@ -132,61 +139,6 @@ var timer = {
             $("#timer").text(timer.clock);
         }
     }
-}
 
-getQuestions();
 
-var questionList = {
-    num: 8,
-    gameOn: function() {
-        if (questionList === 0) {
-             questionList.gameOver();
 
-    } else {
-        $("#start").css("display", "none");
-
-        gameActive = true;
-        timer.begin();
-        questionList.showQuestion();
-    }
-},
-
-showQuestion: function() {
-    questionList.num--;
-
-    $("#question").text(myQuestions[questionList.num].ans[0]);
-    $("#question").text(myQuestions[questionList.num].ans[1]);
-    $("#question").text(myQuestions[questionList.num].ans[2]);
-    $("#question").text(myQuestions[questionList.num].ans[3]);
-},
-
-checkAnswer: function(data) {
-    if (parseInt(data) === myQuestions[questionList.num].ansIndex) {
-        correctAnswer = true;
-        rightAnswer++;
-    } else {
-        correctAnswer = false;
-        wrongAnswer++;
-    }
-},
-
-gameOver: function() {
-    $("#question").css("display", "none");
-    $("#button1").css("display", "none");
-    $("#button2").css("display", "none");
-    $("#button3").css("display", "none");
-    $("#button4").css("display", "none");
-
-    var unAnswered;
-    if ((rightAnswer + wrongAnswer) === 10) {
-        unAnswered = 0;
-    } else {
-        unAnswered = 10 - rightAnswer + wrongAnswer;
-    }
-    $("#unAnswered").text(unAnswered);
-    $("#score").text(rightAnswer + '/' + '8');
-}
-}
-
-timer.begin();
-console.log(wrongAnswer);
